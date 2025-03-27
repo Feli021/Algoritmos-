f037e30c04c0defdd8222c054940d25a449d5f96
Felipe <felidrummond84@gmail.com>
2025-03-27 15:27:49 -0300

    Atualizando

--- week1/Lonely_Integer.py
+++ week1/Lonely_Integer.py
@@ -1,6 +1,19 @@
+import os
+
 def lonelyinteger(a):
-    
-    dicionario = {num: a.count(num) for num in a}
-    for num, qtd in dicionario.items():
-        if qtd == 1:  
+    dicionario = {num: a.count(num) for num in a}  # Criando um dicionário com contagens
+    for num, qtd in dicionario.items():  # Iterando sobre as chaves e valores do dicionário
+        if qtd == 1:  # Se a quantidade for 1, retornamos o número
             return num
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))  # Lendo a lista de inteiros
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+    fptr.close()
--- week1/Plus_Minus.py
+++ week1/Plus_Minus.py
@@ -1,30 +1,20 @@
-def plusMinus(arr):
-   
-    # To know the size of the array
-    total = len(arr)
-    # To count how many positives, negatives, and zeros there are
-    positive_count = 0
-    for x in arr:
-        if x > 0:
-            positive_count = positive_count + 1
-    
+import os
 
-    negative_count = 0
-    for x in arr:
-        if x < 0:
-            negative_count = negative_count + 1
+def plusMinus(arr):
+    total = len(arr)
     
+    # Inicializando as contagens
+    positive_count = sum(1 for x in arr if x > 0)
+    negative_count = sum(1 for x in arr if x < 0)
+    zero_count = total - positive_count - negative_count  # O restante são zeros
     
-    zero_count = 0
-    for x in arr:
-        if x == 0:
-            zero_count = zero_count + 1
+    # Calculando as proporções
+    positive_ratio = positive_count / total
+    negative_ratio = negative_count / total
+    zero_ratio = zero_count / total
     
-    # To calculate the proportions by total
-    positive_ratio = positive_count/total
-    negative_ratio = negative_count/total
-    zero_ratio = zero_count/total
-    # To show the results of the ratios with 6 decimal places
+    # Exibindo as proporções com 6 casas decimais
     print(f'{positive_ratio: .6f}')
     print(f'{negative_ratio: .6f}')
     print(f'{zero_ratio: .6f}')
+
